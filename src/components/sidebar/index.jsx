745dc88dc7e63a97bc8d81b96afc4f59522a053a
Pedro Samuel <pedrosviki1265@gmail.com>
2023-05-31 14:07:58 -0400

    Layout para sacola vazia

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -3,7 +3,7 @@ import {Dock} from 'react-dock';
 import Produto from '../produto/sacola/index.jsx';
 import { useContext, useEffect, useState } from "react";
 import { SacolaContext } from "../../contexts/sacola";
-
+import Sacola from "../../assets/bag.png"
 function Sidebar(){
 
   const [show, setShow] = useState(false);
@@ -26,49 +26,56 @@ function Sidebar(){
   return <Dock position="right" isVisible={show} onVisibleChange={(visible) => {
     setShow(visible);
   }}>
-    <div className="container-fluid h-100 pt-4 sidebar">
-      <h5>Minha Sacola</h5>
+    {
+      sacola.length == 0 ? 
+      <div className="d-flex h-100 flex-column justify-content-center align-items-center text-center">
+        <img src={Sacola} alt="Sacola vazia" />
+        <small className="mt-2 text-secondary">Sua sacola está vazia</small>
+      </div> :
+      <div className="container-fluid h-100 pt-4 sidebar">
+        <h5>Minha Sacola</h5>
 
-      <div className="row produtos">
-        {
-          sacola.map(produto => {
-            return <Produto key={produto} nome={produto.nome}
-                            vlTotal={produto.vlUnitario * produto.qtd}
-                            qtd={produto.qtd}
-                            vlUnitario={produto.vlUnitario}
-                            idCarrinho={produto.idCarrinho}
-                            urlFoto={produto.urlFoto}
-                            onClickRemover={ClickRemover}/>
-          })
-        }
-      </div>
-      <div className="row align-items-end footer">
-        <div className="col-12 d-flex justify-content-between align-items-center">
-          <span>Subtotal</span>
-          <span>{new Intl.NumberFormat('pt-BR', {style:'currency', currency: 'BRL'}).format(subtotalSacola)}</span>
+        <div className="row produtos">
+          {
+            sacola.map(produto => {
+              return <Produto key={produto} nome={produto.nome}
+                              vlTotal={produto.vlUnitario * produto.qtd}
+                              qtd={produto.qtd}
+                              vlUnitario={produto.vlUnitario}
+                              idCarrinho={produto.idCarrinho}
+                              urlFoto={produto.urlFoto}
+                              onClickRemover={ClickRemover}/>
+            })
+          }
         </div>
-        <div className="col-12 d-flex justify-content-between align-items-center mt-2">
-          <div className="input-group">
-            <input type="text" className="form-control" placeholder="Cupom" aria-label="Recipient's username" aria-describedby="button-addon2" />
-            <button className="btn btn-outline-success" type="button" id="button-addon2">Applicar</button>
+        <div className="row align-items-end footer">
+          <div className="col-12 d-flex justify-content-between align-items-center">
+            <span>Subtotal</span>
+            <span>{new Intl.NumberFormat('pt-BR', {style:'currency', currency: 'BRL'}).format(subtotalSacola)}</span>
           </div>
-          <div className="input-group justify-content-end">
-            <span className="d-inline-block text-success">- {new Intl.NumberFormat('pt-BR', {style:'currency', currency: 'BRL'}).format(descontoSacola)}</span>
+          <div className="col-12 d-flex justify-content-between align-items-center mt-2">
+            <div className="input-group">
+              <input type="text" className="form-control" placeholder="Cupom" aria-label="Recipient's username" aria-describedby="button-addon2" />
+              <button className="btn btn-outline-success" type="button" id="button-addon2">Applicar</button>
+            </div>
+            <div className="input-group justify-content-end">
+              <span className="d-inline-block text-success">- {new Intl.NumberFormat('pt-BR', {style:'currency', currency: 'BRL'}).format(descontoSacola)}</span>
+            </div>
           </div>
-        </div>
-        <div className="col-12 d-flex justify-content-between align-items-center mt-2">
-          <span>Taxa de Entrega</span>
-          <span>{new Intl.NumberFormat('pt-BR', {style:'currency', currency: 'BRL'}).format(entregaSacola)}</span>
-        </div>
+          <div className="col-12 d-flex justify-content-between align-items-center mt-2">
+            <span>Taxa de Entrega</span>
+            <span>{new Intl.NumberFormat('pt-BR', {style:'currency', currency: 'BRL'}).format(entregaSacola)}</span>
+          </div>
 
-        <div className="col-12 d-flex justify-content-between align-items-center mt-3">
-          <b>Total</b>
-          <h4>{new Intl.NumberFormat('pt-BR', {style:'currency', currency: 'BRL'}).format(totalSacola)}</h4>
-        </div>
+          <div className="col-12 d-flex justify-content-between align-items-center mt-3">
+            <b>Total</b>
+            <h4>{new Intl.NumberFormat('pt-BR', {style:'currency', currency: 'BRL'}).format(totalSacola)}</h4>
+          </div>
 
-        <button className="btn btn-lg btn-danger rounded-0 align-items-center btn-pedido">Finalizar Pedido</button>
+          <button className="btn btn-lg btn-danger rounded-0 align-items-center btn-pedido">Finalizar Pedido</button>
+        </div>
       </div>
-    </div>
+    }
   </Dock>
 }
 
