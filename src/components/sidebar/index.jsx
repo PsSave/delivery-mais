f9948ee8e131c080ae37cd24958170dbdf964d39
Pedro Samuel <pedrosviki1265@gmail.com>
2023-05-31 17:29:53 -0400

    Aplicando e finalizando cupom de desconto

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -7,21 +7,14 @@ import Sacola from "../../assets/bag.png"
 function Sidebar(){
 
   const [show, setShow] = useState(false);
-  const {sacola, setSacola, subtotalSacola, descontoSacola, totalSacola, entregaSacola} = useContext(SacolaContext);
+  const {sacola, setSacola, subtotalSacola, descontoSacola, totalSacola, entregaSacola, RemoveItemSacola, ValidarCupom,
+          cupom, setCupom, msgSacola, setMsgSacola} = useContext(SacolaContext);
 
   useEffect(() => {
     window.addEventListener('openSidebar', () => {
       setShow(true);
     });
   }, []);
-
-  function ClickRemover(id){
-    const novaSacola = sacola.filter((item, index, array) => {
-      return item.idCarrinho != id;
-    })
-
-    setSacola(novaSacola);
-  }
 
   return <Dock position="right" isVisible={show} onVisibleChange={(visible) => {
     setShow(visible);
@@ -44,7 +37,7 @@ function Sidebar(){
                               vlUnitario={produto.vlUnitario}
                               idCarrinho={produto.idCarrinho}
                               urlFoto={produto.urlFoto}
-                              onClickRemover={ClickRemover}/>
+                              onClickRemover={RemoveItemSacola}/>
             })
           }
         </div>
@@ -55,10 +48,11 @@ function Sidebar(){
           </div>
           <div className="col-12 d-flex justify-content-between align-items-center mt-2">
             <div className="input-group">
-              <input type="text" className="form-control" placeholder="Cupom" aria-label="Recipient's username" aria-describedby="button-addon2" />
-              <button className="btn btn-outline-success" type="button" id="button-addon2">Applicar</button>
+              <input onChange={(e) => setCupom(e.target.value)} value={cupom} type="text" className="form-control" placeholder="Cupom" aria-label="Recipient's username" aria-describedby="button-addon2" />
+              <button onClick={ValidarCupom} className="btn btn-outline-success" type="button" id="button-addon2">Aplicar</button>
             </div>
             <div className="input-group justify-content-end">
+              {msgSacola.length > 0 ? <small className="text-danger me-3">{msgSacola}</small> : null}
               <span className="d-inline-block text-success">- {new Intl.NumberFormat('pt-BR', {style:'currency', currency: 'BRL'}).format(descontoSacola)}</span>
             </div>
           </div>
--- src/contexts/sacola.jsx
+++ src/contexts/sacola.jsx
@@ -1,4 +1,5 @@
 import { createContext, useEffect, useState } from "react";
+import api from "../services/api";
 
   const SacolaContext = createContext({});
 
@@ -8,13 +9,63 @@ import { createContext, useEffect, useState } from "react";
     const [subtotalSacola, setSubtotalSacola] = useState(0);
     const [descontoSacola, setDescontoSacola] = useState(0);
     const [entregaSacola, setEntregaSacola] = useState(0);  
-    const [idCupomSacola, setIdCupomSacola] = useState(0);  
+    const [idCupomSacola, setIdCupomSacola] = useState(0);
+    const [cupom, setCupom] = useState('');
+    const [msgSacola, setMsgSacola] = useState('');
     const [totalSacola, setTotalSacola] = useState(0);  
     const [idEstabelecimentoSacola, setIdEstabelecimentoSacola] = useState(0);  
 
     function AddItemSacola(item){
       setSacola([...sacola, item]);
     }
+
+    function RemoveItemSacola(id){
+      const novaSacola = sacola.filter((item, index, array) => {
+        return item.idCarrinho != id;
+      })
+  
+      setSacola(novaSacola);
+    }
+
+    function ValidarCupom(){
+      setMsgSacola('');
+
+      api.get(`v1/cupons/validacao`, {
+        params: {
+          codCupom: cupom,
+          valor: Math.trunc(subtotalSacola * 100),
+          idEstabelecimento: idEstabelecimentoSacola
+        }
+      })
+      .then(response => {
+        if(response.data){
+          let porcCupom = response.data.porcCupom;
+          let vlCupom = response.data.vlCupom;
+
+          setIdCupomSacola(response.data.idCupom);
+          setDescontoSacola(vlCupom + (subtotalSacola + porcCupom / 100));
+        } else {
+          setIdCupomSacola(0);
+          setDescontoSacola(0);
+          setMsgSacola('Cupom inválido');
+          setTimeout(() => setMsgSacola(''), 3000);
+
+        }
+      }).catch(err => {
+        setIdCupomSacola(0);
+        setDescontoSacola(0);
+        setMsgSacola('Cupom inválido');
+        setTimeout(() => setMsgSacola(''), 3000);
+        console.log(err);
+      })
+
+    }
+
+    useEffect(() => {
+      if(cupom.length > 0) {
+        ValidarCupom();
+      }
+    }, [subtotalSacola])
 
     useEffect(() => {
       let soma = sacola.reduce((a, b) => a + (b.vlUnitario * b.qtd), 0);
@@ -23,13 +74,12 @@ import { createContext, useEffect, useState } from "react";
     }, [sacola]);
 
     useEffect(() => {
-
       setTotalSacola(subtotalSacola - descontoSacola + entregaSacola);
     }, [subtotalSacola, descontoSacola, entregaSacola]);
 
     return <SacolaContext.Provider value={{sacola, setSacola, subtotalSacola, setSubtotalSacola,
                                           descontoSacola, setDescontoSacola, 
-                                          entregaSacola, setEntregaSacola, idCupomSacola, setIdCupomSacola, totalSacola, setTotalSacola, idEstabelecimentoSacola, setIdEstabelecimentoSacola, AddItemSacola}}>
+                                          entregaSacola, setEntregaSacola, idCupomSacola, setIdCupomSacola, totalSacola, setTotalSacola, idEstabelecimentoSacola, setIdEstabelecimentoSacola, AddItemSacola, RemoveItemSacola, ValidarCupom, cupom, setCupom, msgSacola, setMsgSacola}}>
       {props.children}
     </SacolaContext.Provider>
   };
